5c2e424a3fb569e47120e592beae4f527063e7dc
Auros Nexus <aurosnex@gmail.com>
2021-06-05 06:00:03 -0400

    idk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from './index.module.css'
 import Welcome from '../components/Welcome'
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -14,13 +14,13 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
     </header>
-  );
+  )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={``} description="SiraUtil documentation website.">
+    <Layout title={''} description="SiraUtil documentation website.">
       <HomepageHeader />
       <br />
       <main>
