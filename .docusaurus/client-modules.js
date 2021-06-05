f974e7e0770faff22f7b77dcf78accb2b12222e2
Auros Nexus <aurosnex@gmail.com>
2021-06-05 06:03:55 -0400

    remvoe smelly doc saurs folder

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\Auros\\Documents\\Programming\\Beat Saber\\SIRA\\sirautil-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Auros\\Documents\\Programming\\Beat Saber\\SIRA\\sirautil-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Auros\\Documents\\Programming\\Beat Saber\\SIRA\\sirautil-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Auros\\Documents\\Programming\\Beat Saber\\SIRA\\sirautil-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Auros\\Documents\\Programming\\Beat Saber\\SIRA\\sirautil-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Auros\\Documents\\Programming\\Beat Saber\\SIRA\\sirautil-docs\\src\\css\\custom.css"),
-];
